d81af7089775c3476301b5a4083f76a023acd77c
rmateas <rmateas@followone.com>
2024-06-27 20:51:44 +0000

    switched to module

--- dev/scrapers/vinfo/getVInfo.js
+++ dev/scrapers/vinfo/getVInfo.js
@@ -1,24 +1,24 @@
 //Dealer Inspire|DealerOn|template1
-const fs = require('fs');
-const path = require('path');
-const { setTimeout } = require('node:timers/promises');
-const { performance:time } = require('perf_hooks');
 
-const { log, devLog } = require('../sharedSnips/template/logger.js');
-const { getAPI, postAPI } = require('./utils/apiUtils.js');
-const pageNav = require('../sharedSnips/template/navigation.js');
-const comTemp = require('../sharedSnips/scrapers/vehData/getVehInfo.js');
+import { setTimeout } from 'node:timers/promises';
+import { performance as time } from 'perf_hooks';
 
-module.exports = async (worker, page, qp, proxy) => {
-  devLog(worker, 'getVehInfo.js', 'getVehInfo', 'Start');
+import { log } from '../../utils/logger/logger.js';
+import { getAPI, postAPI } from '../../utils/apiUtils.js';
+import { pageNav } from '../../utils/navigation.js';
+import getVehInfo from './utils/getVehInfo.js';
+
+const file = 'getVInfo.js';
+const func = 'getVInfo';
+
+export default getVInfo = async (worker, page, qp, proxy) => {
+  log({file, func, worker, message:'START'});
 
   let VehCardInfoArr = [];
   let APIData = await getAPI(worker, `${qp.host}/vehicle/get/${qp.limit}`);
   
   for(let specs of APIData) {
-    log(worker, 'getVehInfo.js', 'getVehInfo', `Current url: ${specs.url}`);
-    let getVehInfo = fs.existsSync(path.join(__dirname, `../${specs.sellerTemplate}/pageScraper.js`)) ? 
-      require(`../${specs.sellerTemplate}/getVehicleInfo.js`) : comTemp;
+    log({file, func, worker, message:`Current url: ${specs.url}`});
   
     let time1 = time.now();
     try {
@@ -33,11 +33,11 @@ module.exports = async (worker, page, qp, proxy) => {
       if(getSpecs.errArr.length){
         specs.vehInfoErrs = {errDate: new Date(), errArr:getSpecs.errArr};
       }
-    } catch (e) {
-      specs.scrapeFail = {errDate: new Date(), errMessage: e.message, errStack: e.stack};
-      log(worker, 'getVehInfo.js', 'getVehInfo', `FAIL | Error at ${specs.url}`, e);
+    } catch (error) {
+      specs.scrapeFail = {errDate: new Date(), errMessage: error.message, errStack: error.stack};
+      log({file, func, worker, message:`FAIL | Error at ${specs.url}`, error});
     } finally {
-      log(worker, 'getVehInfo.js', 'getVehInfo', `Scrape Time : ${(time.now() - time1).toString().split(".")[0]}ms | Vehicle : ${specs.url}`, specs);
+      log({file, func, worker, message:`Scrape Time : ${(time.now() - time1).toString().split(".")[0]}ms | Vehicle : ${specs.url}`, obj:specs});
       VehCardInfoArr.push(specs);
       let time2 = time.now();
       if((time2 - time1) < 7000){
