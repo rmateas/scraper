749a3fa7d7567a372eb80a88d7be1a9dbbc7b98c
rmateas <99758865+rmateas@followone.com>
2024-07-18 10:42:00 -0700

    work

--- dev/scrapers/pag/utils/findAllPaginationUrls.js
+++ dev/scrapers/pag/utils/findAllPaginationUrls.js
@@ -12,77 +12,106 @@ const func = 'findAllPaginationUrls';
 export const findAllPaginationUrls = async (page, worker) => {
   log({level:'debug', file, func, worker, message:'START'});
 
-  let paginationUrls = [];
-  let possibleUrl;
-  let iterator;
-  let urlMatch;
-  let replaceRx;
+  let pagination = {
+    allPaginationUrlsArr: [],
+    possiblePaginationFilteredArr: []
+  }
+
   let urlMatchRx = /\b_?(p(t|g|age(_no)?)?|start)(-|=)\d\d?\d?\d?\d?/;
   let urlCardLimitRx = /&\b(pager|limit)=\d\d?\d?|\b(pager|limit)=\d\d?\d?\d?\d?&/;
-  let allUrls = [];
   try {
-    await page.$$eval('a', url => {urlMatchRx.test(url.href) && !/\/#/.test(url.href) && allUrls.push(url.href)})
+    await page.$$eval('a', url => {urlMatchRx.test(url.href) && !/\/#/.test(url.href) && pagination.allPaginationUrlsArr.push(url.href)})
   } catch (error) {
     await log({level:'error', file, func, worker, message:'ERROR GETTING PAGINATION URLS', error});
+    return pagination;
   }
+  if(!pagination.allPaginationUrlsArr.length){return pagination;}
+  pagination.allPaginationUrlsArr = [...new Set(pagination.allPaginationUrlsArr)].sort((a,b)=>a.length-b.length);
+
+  pagination.allPaginationUrlsArr.forEach(url => {
+    //FIND POSSIBLE COMMON STRING FOR PAGINATION AND SEE IF IT EXISTS IN OTHER URLS
+    let initialUrlMatch = url.match(urlMatchRx)[0];
+    let iterator = +initialUrlMatch.replace(/[^\d]/g, '');
+    let urlMatch = initialUrlMatch.replace(/\d/g, '');
+    let replaceRx = new RegExp(`(?<=(\\b${urlMatch}))\\d\\d?\\d?\\d?\\d?`);
+    let possibleUrl = url.replace(replaceRx, '~~~');
+    // possibleUrl = !/^http/.test(url) ? location.origin+possibleUrl : possibleUrl;
+    
+    // FIND CARD LIMIT PER PAGE IF EXISTS AND PART OF URL
+    // if(urlCardLimitRx.test(possibleUrl)){
+    //   let limitMatch = url.match(urlCardLimitRx)[0];
+    //   let cardLimit = +limitMatch.replace(/[^\d]/g, '');
+    //   let limitMatched = limitMatch.replace(/\d\d?\d?\d?\d?/, '@@@');
+    //   limitMatched = /&$/.test(limitMatched) ? `&${limitMatched.replace(/&/,'')}` : limitMatched;
+    //   let limitUrl = possibleUrl.replace(limitMatch, '');
+    //   let exists = pagination.possiblePaginationFilteredArr.find(el => {
+    //     if(el.url == limitUrl){
+    //       el.limitMatch = limitMatched;
+    //       el.limit = 'limit' in el ? el.limit < cardLimit ? cardLimit: el.limit : cardLimit;
+    //     }
+    //     return true;
+    //   });
+    //   !exists && pagination.possiblePaginationFilteredArr.push({url:limitUrl, iterators:[iterator], limit:cardLimit, limitMatch:limitMatched});
+    // } else {
+      let exists = pagination.possiblePaginationFilteredArr.find(el => el.url === possibleUrl && el.iterators.push(iterator));
+      !exists && pagination.possiblePaginationFilteredArr.push({url:possibleUrl, iterators:[iterator]});
+    // }
+  });
+  // for(let url of pagination.possiblePaginationFilteredArr){
+  //   if('limit' in url){
+  //     url = {url: `${url.url}${url.limitMatch.replace(/@@@/, url.limit)}`, iterators:url.iterators}
+  //   }
+  // }
+
+  return pagination;
+  
 
 
 
 
 
-  try{
-
-
-
-
-
-
-
-
-
-
-
-    let posPagUrls =  await page.evaluate(() => {
-      document.querySelectorAll('a').forEach(url => {urlMatchRx.test(url.href) && !/\/#/.test(url.href) && allUrls.push(url.href)});
-      allUrls = [...new Set(allUrls)];
-      allUrls.sort((a,b)=>{return a.length-b.length});
-      allUrls.forEach(url => {
-        urlMatch = url.match(urlMatchRx)[0];
-        iterator = +urlMatch.replace(/[^\d]/g, '');
-        urlMatch = urlMatch.replace(/\d/g, '');
-        replaceRx = new RegExp(`(?<=(\\b${urlMatch}))\\d\\d?\\d?\\d?\\d?`);
-        possibleUrl = url.replace(replaceRx, '~~~');
-        possibleUrl = !/^http/.test(url) ? location.origin+possibleUrl : possibleUrl;
-        if(urlCardLimitRx.test(possibleUrl)){
-          let limitMatch = url.match(urlCardLimitRx)[0];
-          let cardLimit = +limitMatch.replace(/[^\d]/g, '');
-          let limitMatched = limitMatch.replace(/\d\d?\d?\d?\d?/, '@@@');
-          limitMatched = /&$/.test(limitMatched) ? `&${limitMatched.replace(/&/,'')}` : limitMatched;
-          let limitUrl = possibleUrl.replace(limitMatch, '');
-          let exists = paginationUrls.find(el => {
-            if(el.url == limitUrl){
-              el.limitMatch = limitMatched;
-              el.limit = 'limit' in el ? el.limit < cardLimit ? cardLimit: el.limit : cardLimit;
-            }
-            return true;
-          });
-          !exists && paginationUrls.push({url:limitUrl, iterators:[iterator], limit:cardLimit, limitMatch:limitMatched});
-        } else {
-          let exists = paginationUrls.find(el => el.url === possibleUrl && el.iterators.push(iterator));
-          !exists && paginationUrls.push({url:possibleUrl, iterators:[iterator]});
-        }
-      });
-      for(let url of paginationUrls){
-        if('limit' in url){
-          url = {url: `${url.url}${url.limitMatch.replace(/@@@/, url.limit)}`, iterators:url.iterators}
-        }
-      }
-      return paginationUrls;
-    });
-    log({level:'debug', file, func:'findAllPaginationUrls', worker, message:'SUCCESS | Exiting findAllPaginationUrls', obj:posPagUrls});
-    return posPagUrls;
-  } catch (error) {
-    await log({level:'error', file, func:'findAllPaginationUrls', worker, message:'FAIL | Exiting findAllPaginationUrls', error});
-    throw new Error(`Error getting possible pagination URLs`);
-  }
+  // try{
+  //   let posPagUrls =  await page.evaluate(() => {
+  //     document.querySelectorAll('a').forEach(url => {urlMatchRx.test(url.href) && !/\/#/.test(url.href) && allUrls.push(url.href)});
+  //     allUrls = [...new Set(allUrls)];
+  //     allUrls.sort((a,b)=>{return a.length-b.length});
+  //     allUrls.forEach(url => {
+  //       urlMatch = url.match(urlMatchRx)[0];
+  //       iterator = +urlMatch.replace(/[^\d]/g, '');
+  //       urlMatch = urlMatch.replace(/\d/g, '');
+  //       replaceRx = new RegExp(`(?<=(\\b${urlMatch}))\\d\\d?\\d?\\d?\\d?`);
+  //       possibleUrl = url.replace(replaceRx, '~~~');
+  //       possibleUrl = !/^http/.test(url) ? location.origin+possibleUrl : possibleUrl;
+  //       if(urlCardLimitRx.test(possibleUrl)){
+  //         let limitMatch = url.match(urlCardLimitRx)[0];
+  //         let cardLimit = +limitMatch.replace(/[^\d]/g, '');
+  //         let limitMatched = limitMatch.replace(/\d\d?\d?\d?\d?/, '@@@');
+  //         limitMatched = /&$/.test(limitMatched) ? `&${limitMatched.replace(/&/,'')}` : limitMatched;
+  //         let limitUrl = possibleUrl.replace(limitMatch, '');
+  //         let exists = paginationUrls.find(el => {
+  //           if(el.url == limitUrl){
+  //             el.limitMatch = limitMatched;
+  //             el.limit = 'limit' in el ? el.limit < cardLimit ? cardLimit: el.limit : cardLimit;
+  //           }
+  //           return true;
+  //         });
+  //         !exists && paginationUrls.push({url:limitUrl, iterators:[iterator], limit:cardLimit, limitMatch:limitMatched});
+  //       } else {
+  //         let exists = paginationUrls.find(el => el.url === possibleUrl && el.iterators.push(iterator));
+  //         !exists && paginationUrls.push({url:possibleUrl, iterators:[iterator]});
+  //       }
+  //     });
+  //     for(let url of paginationUrls){
+  //       if('limit' in url){
+  //         url = {url: `${url.url}${url.limitMatch.replace(/@@@/, url.limit)}`, iterators:url.iterators}
+  //       }
+  //     }
+  //     return paginationUrls;
+  //   });
+  //   log({level:'debug', file, func:'findAllPaginationUrls', worker, message:'SUCCESS | Exiting findAllPaginationUrls', obj:posPagUrls});
+  //   return posPagUrls;
+  // } catch (error) {
+  //   await log({level:'error', file, func:'findAllPaginationUrls', worker, message:'FAIL | Exiting findAllPaginationUrls', error});
+  //   throw new Error(`Error getting possible pagination URLs`);
+  // }
 }
